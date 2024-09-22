59ea7cdc10f6510198d0d49c503a68b33deccd84
Kevin French <kfrench@gsinet.net>
2024-09-22 15:18:29 -0400

    Add transcription

--- API_Examples.py
+++ API_Examples.py
@@ -87,5 +87,16 @@ response = client.moderations.create(
 # Print the category scores
 print(response.results[0].category_scores)
 
-# 
+# Creating transcripts----
+
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Open the openai-audio.mp3 file
+audio_file = open(openai-audio.mp3, "rb")
+
+# Create a transcript from the audio file
+response = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+
+# Extract and print the transcript text
+print(response)
 
