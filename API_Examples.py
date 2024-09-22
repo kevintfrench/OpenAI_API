2b4ca52e5ee02d08e690fda1cf7b1365d20cb7d2
Kevin French <kfrench@gsinet.net>
2024-09-22 15:20:27 -0400

    Fix quotes

--- API_Examples.py
+++ API_Examples.py
@@ -92,7 +92,7 @@ print(response.results[0].category_scores)
 client = OpenAI(api_key="<OPENAI_API_TOKEN>")
 
 # Open the openai-audio.mp3 file
-audio_file = open(openai-audio.mp3, "rb")
+audio_file = open("openai-audio.mp3", "rb")
 
 # Create a transcript from the audio file
 response = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
