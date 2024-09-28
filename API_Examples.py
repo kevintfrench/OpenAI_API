2f10ab4d768487770065c214df9916accdd8aec3
Kevin French <kfrench@gsinet.net>
2024-09-28 13:10:56 -0400

    add audio file

--- API_Examples.py
+++ API_Examples.py
@@ -1,10 +1,10 @@
-# Basic query
+# Basic query--------------
 client = OpenAI(api_key="<OPENAI_API_TOKEN>")
 
 prompt="""Summarize the following text into two concise bullet points:
 Investment refers to the act of committing money or capital to an enterprise with the expectation of obtaining an added income or profit in return. There are a variety of investment options available, including stocks, bonds, mutual funds, real estate, precious metals, and currencies. Making an investment decision requires careful analysis, assessment of risk, and evaluation of potential rewards. Good investments have the ability to produce high returns over the long term while minimizing risk. Diversification of investment portfolios reduces risk exposure. Investment can be a valuable tool for building wealth, generating income, and achieving financial security. It is important to be diligent and informed when investing to avoid losses."""
 
-# Create a request to the Completions endpoint
+# Create a request to the Completions endpoint---------------
 response = client.completions.create(
   temperature=0,
   model="gpt-3.5-turbo-instruct",
@@ -18,7 +18,7 @@ print(response.choices[0].text)
 
 client = OpenAI(api_key="<OPENAI_API_TOKEN>")
 
-# Create a request to the Chat Completions endpoint
+# Create a request to the Chat Completions endpoint--------------
 response = client.chat.completions.create(
   model="gpt-4o-mini",
   max_tokens=150,
@@ -33,7 +33,7 @@ response = client.chat.completions.create(
 # Extract and print the assistant's text response
 print(response.choices[0].message.content)
 
-# chat completions
+# chat completions--------------
 
 client = OpenAI(api_key="<OPENAI_API_TOKEN>")
 
@@ -45,7 +45,7 @@ heights = heights_dict.values()
 print(np.mean(heights))
 """
 
-# Create a request to the Chat Completions endpoint
+# Create a request to the Chat Completions endpoint---------
 response = client.chat.completions.create(
   model="gpt-4o-mini",
   messages=[
@@ -57,7 +57,7 @@ response = client.chat.completions.create(
 
 print(response.choices[0].message.content)
 
-# This code demonstrates how to structure messages for the API, including a system message, user questions, and assistant # # responses, to facilitate in-context learning and generate accurate, context-aware responses from the model.
+# This code demonstrates how to structure messages for the API, including a system message, user questions, and assistant # # responses, to facilitate in-context learning and generate accurate, context-aware responses from the model.---------
 
 client = OpenAI(api_key="<OPENAI_API_TOKEN>")
 
@@ -73,7 +73,7 @@ response = client.chat.completions.create(
 
 print(response.choices[0].message.content)
 
-# Query OpenAI text moderation
+# Query OpenAI text moderation----------------
 
 client = OpenAI(api_key="<OPENAI_API_TOKEN>")
 
