fb7bc1d1c0f749cc56cb4fd3bb32fecd304500e9
Kevin French <kfrench@gsinet.net>
2024-09-29 11:35:37 -0400

    Add Translations

--- API_Examples.py
+++ API_Examples.py
@@ -100,3 +100,17 @@ response = client.audio.transcriptions.create(model="whisper-1", file=audio_file
 # Extract and print the transcript text
 print(response.text)
 
+
+# Translations ------
+
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Open the audio.m4a file
+audio_file = open("audio.m4a", "rb")
+
+# Create a translation from the audio file
+response = client.audio.translations.create(model="whisper-1", file=audio_file)
+
+# Extract and print the translated text
+print(response.text)
+
