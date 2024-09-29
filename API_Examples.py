bb14017e37474c89281c3e353d45ae15828cbc84
Kevin French <kfrench@gsinet.net>
2024-09-29 11:40:03 -0400

    Add translation prompt

--- API_Examples.py
+++ API_Examples.py
@@ -114,3 +114,17 @@ response = client.audio.translations.create(model="whisper-1", file=audio_file)
 # Extract and print the translated text
 print(response.text)
 
+
+# Translation with prompt -------
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Open the audio.wav file
+audio_file = open("audio.wav", "rb")
+
+# Write an appropriate prompt to help the model
+prompt = "The audio relates to a recent World Bank report"
+
+# Create a translation from the audio file
+response = client.audio.translations.create(model="whisper-1", file=audio_file, prompt=prompt)
+
+print(response.text)
