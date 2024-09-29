0a74effbefc3308ca64c53e5652aace3c86cd2f7
Kevin French <kfrench@gsinet.net>
2024-09-29 11:50:58 -0400

    Add chaining models example

--- API_Examples.py
+++ API_Examples.py
@@ -128,3 +128,20 @@ prompt = "The audio relates to a recent World Bank report"
 response = client.audio.translations.create(model="whisper-1", file=audio_file, prompt=prompt)
 
 print(response.text)
+
+
+# Chaining models in audio translations example-------------
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Open the audio.wav file
+audio_file = open("audio.wav", "rb")
+
+# Create a transcription request using audio_file
+audio_response = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+
+# Create a request to the API to identify the language spoken
+chat_response = client.chat.completions.create(model="gpt-4o-mini",   messages=[
+    {"role": "system", "content": "You are a languages specialist."},
+    {"role": "user", "content": "Identify the language used in the following text: " + audio_response.text}
+  ])
+print(chat_response.choices[0].message.content)
