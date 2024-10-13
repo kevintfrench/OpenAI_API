417d81c982e9c54ab9994d7b111cc54a190c1321
Kevin French <kfrench@gsinet.net>
2024-10-13 14:19:07 -0400

    Update api

--- API_Examples.py
+++ API_Examples.py
@@ -250,3 +250,39 @@ response = client.chat.completions.create(
   temperature = 0
 )
 print(response.choices[0].message.content)
+
+# Multistep prompt
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Create a prompt detailing steps to plan the trip
+prompt = """
+     Help me plan a beach vacation.
+     Step 1 - Specify four potential locations for beach vacations
+     Step 2 - State some accommodation options in each
+     Step 3 - State activities that could be done in each
+     Step 4 - Evaluate the pros and cons for each destination
+    """
+
+response = get_response(prompt)
+print(response)
+
+# analyzing solutions
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+code = '''
+def calculate_rectangle_area(length, width):
+    area = length * width
+    return area
+'''
+
+# Create a prompt that analyzes correctness of the code
+prompt = f"""
+     Analyze the correctness of the function delimited by triple backticks according to the following criteria:
+      1- It should have correct syntax
+      2- The function should receive only 2 inputs
+      3- The function should return only one output
+      ```{code}```
+    """
+
+response = get_response(prompt)
+print(response)
