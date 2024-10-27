c81e33639e4d3f5ecff4946cf65212025fb79ef4
Kevin French <kfrench@gsinet.net>
2024-10-27 16:19:37 -0400

    Add dual prompt

--- API_Examples.py
+++ API_Examples.py
@@ -444,3 +444,19 @@ prompt = f"""Infer the Python function that maps the inputs to the outputs in th
 
 response = get_response(prompt)
 print(response)
+
+# dual-prompt get_response() function
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+def get_response(system_prompt, user_prompt):
+  # Assign the role and content for each message
+  messages = [{"role": "system", "content": system_prompt},
+      		  {"role": "user", "content": user_prompt}]  
+  response = client.chat.completions.create(
+      model="gpt-4o-mini", messages= messages, temperature=0)
+  
+  return response.choices[0].message.content
+
+# Try the function with a system and user prompts of your choice 
+response = get_response("system_prompt", "user_prompt")
+print(response)
