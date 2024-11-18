5cf4af72e4e0359f0404f07628e9979f338893b5
Kevin French <kfrench@gsinet.net>
2024-11-18 14:37:03 -0500

    add comparison pipelines

--- API_Examples.py
+++ API_Examples.py
@@ -623,3 +623,17 @@ distil_pipeline = pipeline(task="sentiment-analysis", model="distilbert-base-unc
 
 # Predict the sentiment
 distil_output = distil_pipeline(input)
+
+# Comparing pipelines
+# Create the pipeline
+distil_pipeline = pipeline(task="sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+
+# Predict the sentiment
+distil_output = distil_pipeline(input)
+
+# Create the second pipeline and predict the sentiment
+bert_pipeline = pipeline(task="sentiment-analysis", model="kwang123/bert-sentiment-analysis")
+bert_output = bert_pipeline(input)
+
+print(f"Bert Output: {bert_output[0]['label']}")
+print(f"Distil Output: {distil_output[0]['label']}")
